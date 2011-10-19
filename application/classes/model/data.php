0ee4b780ed43605ddfe74914b7dfb70193b82f9f
Martin Zoellner <ragchuck@gmail.com>
2011-10-19 23:27:12 +0200

    cleanup

---
+++ application/classes/model/data.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('SYSPATH') or die('No direct script access.');
+/*
+ *
+ *
+ */
+
+/**
+ * Description of data
+ *
+ * @author Martin Zoellner <ragchuck at gmail.com>
+ */
+class data {
+
+}
+
+?>
