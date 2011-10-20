5f8dd4e370f023b3683756c9c47144e94707a3b2
Martin Zoellner <ragchuck@gmail.com>
2011-10-21 01:00:44 +0200

    changes 20.10.2011

---
+++ application/classes/controller/import.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Description of Controller_Import
+ *
+ * @author Martin Zoellner <ragchuck at gmail.com>
+ */
+class Controller_Import extends Controller {
+
+	
+
+
+}
